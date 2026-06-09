8ecf4ea72bc6bfaaa63e48caf67ef80d20c38c97
Yosafat Sanjaya Widjaja <yosafatsanjaya04@gmail.com>
2026-06-09 18:01:52 +0700

    Update BIM_Main.py

--- BIM_Main.py
+++ BIM_Main.py
@@ -12,16 +12,20 @@ class BIM_Main:
         self.utils = Utils()
         self.invertedIndex = self.utils.makeInvertedIndex()
         self.bm25 = BM25(k1=1.5, b=0.75)
-        self.two_poisson = TwoPoisson(max_iter=10)
+        self.two_poisson = TwoPoisson(k=1.2)
 
     def main(self):
         while(True):
             query = self.getQuery()
+            if not query:
+                continue
+                
             query_tokens = self.utils.tokenize(query)
             query_stems = self.ps.stem(query_tokens)
             query_posting_lists = self.get_query_posting_lists(query_stems)
-
+            #rel = self.build_relDQ_TwoPoisson(query_posting_lists, query_stems)
             rel = self.build_relDQ(query_posting_lists)
+    
             ranked_docs = self.rank_rel(rel)
             self.print_output(ranked_docs, query)
         
@@ -58,44 +62,42 @@ class BIM_Main:
 
         return rel
     
-    def build_relDQ_BM25(self, query_posting_lists, query_stems):
+def build_relDQ_BM25(self, query_posting_lists, query_stems):
         rel = defaultdict(float)
         N = self.utils.get_N()
         avgdl = self.utils.get_avg_document_length()
 
         for stem, posting_list in zip(query_stems, query_posting_lists):
-            if not posting_list: continue
+            if not posting_list: 
+                continue
             Nt = self.utils.get_Nt(posting_list)
 
             for d in posting_list:
                 tf = self.utils.get_tf(stem, d)
                 dl = self.utils.get_document_length(d)
-                rel[d] += self.bm25.calculate_score(tf, N, Nt, dl, avgdl)
-
+                score = self.bm25.calculate_score(tf, N, Nt, dl, avgdl)
+                rel[d] += score
         return rel
 
-    def build_relDQ_TwoPoisson(self, query_posting_lists, query_stems):
+def build_relDQ_TwoPoisson(self, query_posting_lists, query_stems):
         rel = defaultdict(float)
         N = self.utils.get_N()
 
         for stem, posting_list in zip(query_stems, query_posting_lists):
-            if not posting_list: continue
+            if not posting_list: 
+                continue
             Nt = self.utils.get_Nt(posting_list)
-            
-            # Ambil frekuensi term untuk estimasi parameter
-            tf_list = [self.utils.get_tf(stem, d) for d in range(N)]
-            lambda_1, lambda_2, pi = self.two_poisson.estimate_parameters(tf_list)
             
             for d in posting_list:
                 tf = self.utils.get_tf(stem, d)
-                rel[d] += self.two_poisson.calculate_score(tf, lambda_1, N, Nt)
-
+                score = self.two_poisson.calculate_score(tf, N, Nt)
+                rel[d] += score
         return rel
     
-    def rank_rel(self, rel):
+def rank_rel(self, rel):
         return sorted(rel, key=rel.get, reverse=True)
     
-    def print_output(self, ranked_docs, query):
+def print_output(self, ranked_docs, query):
         print("=====================================================================================================================================")
         print(f"Berdasarkan query: {query}, ")
         print(f"dokumen diretrieve terurut dari yg paling relevan adalah: {ranked_docs}")
